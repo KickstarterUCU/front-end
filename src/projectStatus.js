5e403e144dee10b0f7237a1fbd213b909f2e9c51
Matvii Kovtun <matvii.kovtun@eleks.com>
2018-12-19 16:23:55 +0200

    bug fixes

--- src/projectStatus.js
+++ src/projectStatus.js
@@ -1,6 +1,12 @@
 export const projectStatus = (contract, id, entryPoint) => {
     contract.projectStatus.call(id, (err, res) => {
-        if (!err) renderProjectStatus(res);
+        if (!err) {
+            renderProjectStatus({
+                address: res[0],
+                gatheredAmount: res[1].toNumber(),
+                requiredAmount: res[2].toNumber()
+            });
+        }
     });
 
 
