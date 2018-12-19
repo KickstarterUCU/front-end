677fb06898ab3633ffb542fa4a5826309555f67b
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-12-19 16:58:23 +0200

    render project

--- src/projectStatus.js
+++ src/projectStatus.js
@@ -13,6 +13,11 @@ export const projectStatus = (contract, id, entryPoint) => {
     const renderProjectStatus = (res) => {
         console.log(res);
         console.log(entryPoint);
+    let all_projects = entryPoint.querySelector(".all_projects");
+        all_projects.innerHTML = "<div class='project'>" +
+            "<h2 class='project__owner'>" + res['address']+"</h2>" +
+            "<img class='project__img' src='https://cdn-images-1.medium.com/max/1131/1*9Bz1AgYPjC5snJb4395rGw.jpeg'>" +
+            "<p class='project__gathered'>Gathered amount: " +res['gatheredAmount'] + "</p>" +
+            "<p class='project__required'>Required amount: " +res['requiredAmount'] + "</p></div>";
     }
-
 };
