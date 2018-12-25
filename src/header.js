0bf57cb3fbe1a05596a2a4348443c5f42fdbf8e0
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-12-25 21:54:33 +0200

    changes

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ export const renderHeader = () =>{
 
 const header = () => {
     return `<header class="kickstarter-header">
-                <a href="index.html" class="kickstarter-header__title"><h1 >Kickstarter</h1></a>
+                <a href="index.html" class="kickstarter-header__title"><h1 >UCUstarter</h1></a>
                 <button id="add-btn" class="project__button project__button_add">Create project</button>
             </header>`
 }
